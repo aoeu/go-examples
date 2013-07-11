eaa54298b23806ad59c0833aae02e37fceb13e22
Travis <tmcdemus@gmail.com>
2013-07-11 16:30:33 -0400

    Final(ish) version.

--- Workers.go
+++ Workers.go
@@ -6,22 +6,26 @@ import (
 )
 
 type Manager struct {
-	FreeWorkers chan *Worker
-	Tasks       chan int
-	Results     chan int64
-	LastResultTime int64 // Unix timestamp.
-	TimeoutLength int64 // Number of seconds.
-	Quit        chan bool
+	AvailableWorkers chan *Worker
+	Tasks            chan int
+	Results          chan int64
+	LastResultTime   int64 // Unix timestamp.
+	TimeoutLength    int64 // Number of seconds.
+	Quit             chan bool
 }
 
 func NewManager(numWorkers int, timeoutLength int64) Manager {
 	m := Manager{}
 	m.TimeoutLength = timeoutLength
 	m.LastResultTime = time.Now().Unix()
-	m.FreeWorkers = make(chan *Worker, numWorkers)
+	m.AvailableWorkers = make(chan *Worker, numWorkers)
 	m.Tasks = make(chan int)
 	m.Results = make(chan int64)
 	m.Quit = make(chan bool, 1)
+	// Create some workers.
+	for i := 0; i < numWorkers; i++ {
+		m.AvailableWorkers <- &Worker{i, m.Results, m.AvailableWorkers}
+	}
 	return m
 }
 
@@ -42,7 +46,7 @@ func (m *Manager) Manage() {
 func (m *Manager) Dispatch(task int) {
 	fmt.Println("Dispatching task:", task)
 	select {
-	case worker := <-m.FreeWorkers:
+	case worker := <-m.AvailableWorkers:
 		go worker.Work(task)
 	case <-m.Quit:
 		m.Quit <- true
@@ -51,13 +55,13 @@ func (m *Manager) Dispatch(task int) {
 }
 
 func (m *Manager) Process(result int64) {
-	fmt.Println("Got result at:", result)
+	fmt.Println("Got result at:", result, " (Unix time)")
 	m.LastResultTime = int64(result)
 }
 
 func (m *Manager) Timeout(seconds int) {
 	for {
-		if time.Now().Unix() - m.LastResultTime > int64(seconds) {
+		if time.Now().Unix()-m.LastResultTime > int64(seconds) {
 			fmt.Println("Timing out.")
 			m.Quit <- true
 			return
@@ -68,27 +72,25 @@ func (m *Manager) Timeout(seconds int) {
 }
 
 type Worker struct {
-	Id          int
-	Results     chan int64
-	FreeWorkers chan *Worker
+	Id               int
+	Results          chan int64
+	AvailableWorkers chan *Worker
 }
 
 func (w *Worker) Work(task int) {
 	fmt.Printf("Worker %d is working on task %d\n", w.Id, task)
 	time.Sleep(1 * time.Second)
 	w.Results <- time.Now().Unix()
-	w.FreeWorkers <- w    // put self on the shared availability "queue"
+	w.AvailableWorkers <- w // put self on the shared availability "queue"
 }
 
 func main() {
 	// Initialize.
 	numWorkers := 3
 	manager := NewManager(numWorkers, 2)
-	for i := 0; i < numWorkers; i++ { 
-		manager.FreeWorkers <- &Worker{i, manager.Results, manager.FreeWorkers}
-	}
+
 	// Run concurrently.
-	go manager.Manage()  
+	go manager.Manage()
 	for i := 1; i <= 10; i++ {
 		manager.Tasks <- i
 	}
